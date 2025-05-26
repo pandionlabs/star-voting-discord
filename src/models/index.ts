8c658cf1912de8eeff4a32846d3d8c7956b38b1c
Simone Massaro <simone.massaro@mone27.net>
2025-05-26 21:10:14 +0200

    moved db to data folder

--- src/models/index.ts
+++ src/models/index.ts
@@ -126,7 +126,7 @@ class Vote
 // Initialize Sequelize with SQLite
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: path.join(__dirname, "../../database.sqlite"),
+  storage: path.join(__dirname, "../../data/database.sqlite"),
   logging: console.log,
 });
 

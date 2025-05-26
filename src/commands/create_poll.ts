1bac3994b8516a9e765caf53b75abe3654b827ae
Simone Massaro <simone.massaro@mone27.net>
2025-05-26 19:16:37 +0200

    refactor attempt - npm run start works but the bot doesn't seem to work anymore

--- src/commands/create_poll.ts
+++ src/commands/create_poll.ts
@@ -8,8 +8,10 @@ import {
 } from "discord.js";
 import { Poll, Option, sequelize } from "../models/index";
 import { emojiNumberMap } from "../utils";
+import { StringSelectMenuInteraction } from "discord.js";
+import { Vote } from "../models/index";
 
-export const data = new SlashCommandBuilder()
+export const createPollCommand = new SlashCommandBuilder()
   .setName("create_poll")
   .setDescription("Create a STAR voting poll")
   .addStringOption((option) =>
@@ -49,7 +51,7 @@ export const data = new SlashCommandBuilder()
       .setRequired(false),
   );
 
-export async function execute(
+export async function createPollCallback(
   interaction: ChatInputCommandInteraction,
 ): Promise<void> {
   const question = interaction.options.getString("question", true);
@@ -151,3 +153,47 @@ export async function execute(
     components: components,
   });
 }
+
+/**
+ * Handle star voting select menu interactions
+ * This function is called when a user selects a star rating for an option
+ */
+export const votingSelectHandler = async (
+  interaction: StringSelectMenuInteraction,
+): Promise<void> => {
+  // Get the option ID from the custom ID
+  // Format: option_123 where 123 is the option ID
+  const customId = interaction.customId;
+  const optionId = parseInt(customId.split("_")[1]);
+  // Get the selected star value
+  const stars = parseInt(interaction.values[0]);
+  const userId = interaction.user.id;
+
+  // Check if the user already voted for this option
+  const existingVote = await Vote.findOne({
+    where: {
+      userId: userId,
+      optionId: optionId,
+    },
+  });
+  if (existingVote) {
+    // Update existing vote
+    await existingVote.update({ stars: stars });
+    await interaction.reply({
+      content: `You changed your vote to ${stars} star${stars !== 1 ? "s" : ""} for: ${(await existingVote.getOption()).format()}`,
+      ephemeral: true,
+    });
+  } else {
+    // Create new vote
+    await Vote.create({
+      userId: userId,
+      optionId: optionId,
+      stars: stars,
+    });
+
+    await interaction.reply({
+      content: `You voted with ${stars} star${stars !== 1 ? "s" : ""}!`,
+      ephemeral: true,
+    });
+  }
+};
